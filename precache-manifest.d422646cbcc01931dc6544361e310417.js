3fba44bf4f3a59b449728453587bf43826ed11fb
Julian Ranieri <jumpingafterrain@gmail.com>
2020-09-11 22:13:54 -0700

    Updates

---
+++ precache-manifest.d422646cbcc01931dc6544361e310417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fa533d1c19a494872fba4c1c541247",
+    "url": "/reactpractice/index.html"
+  },
+  {
+    "revision": "0a721bd7e3a8275f7278",
+    "url": "/reactpractice/static/js/2.92524a57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactpractice/static/js/2.92524a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ae9168e27cf182e899",
+    "url": "/reactpractice/static/js/main.ad9256f9.chunk.js"
+  },
+  {
+    "revision": "7f9d72772985bf8012cb",
+    "url": "/reactpractice/static/js/runtime-main.a9e6da73.js"
+  }
+]);
