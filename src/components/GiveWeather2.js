192088003ce5d6963cfa076b75280f269580a841
Julian Ranieri <jumpingafterrain@gmail.com>
2020-09-17 17:20:56 -0700

    new updated version not sloppy

---
+++ src/components/GiveWeather2.js
@@ -0,0 +1,152 @@
+import React from 'react'
+
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=San%20Francisco&units=imperial&appid=6b04193aa2d1531aa6072e2ba7eca3c8';
+
+class GiveWeather2 extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            tog: 'off',
+            loading: true
+        }
+    this.myClick = this.myClick.bind(this)
+    }
+
+    componentDidMount() {
+        
+    }
+
+myClick() {
+    console.log(this.state.tog)
+    if(this.state.tog === "off") {
+        this.setState({
+            tog: 'on'
+        })
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    name: [data['name']],
+                    coordlon: [data['coord']['lon']],
+                    coordlat: [data['coord']['lat']],
+                    weatherid: [data['weather'][0]['id']],
+                    weathermain: [data['weather'][0]['main']],
+                    weatherdescription: [data['weather'][0]['description']],
+                    weathericon: [data['weather'][0]['icon']],
+                    maintemp: [data['main']['temp']],
+                    mainfeelslike: [data['main']['feels_like']],
+                    maintempmin: [data['main']['temp_min']],
+                    maintempmax: [data['main']['temp_max']],
+                    mainpressure: [data['main']['pressure']],
+                    mainhumidity: [data['main']['humidity']],
+                    visibility: [data['visibility']],
+                    windspeed: [data['wind']['speed']],
+                    winddeg: [data['wind']['deg']],
+                    cloudsall: [data['clouds']['all']],
+                    dt: [data['dt']],
+                    systype: [data['sys']['type']],
+                    sysid: [data['sys']['id']],
+                    syscountry: [data['sys']['country']],
+                    syssunrise: [data['sys']['sunrise']],
+                    syssunset: [data['sys']['sunset']],
+                    timezone: [data['timezone']],
+                    thisid: [data['id']],
+                    cod: [data['cod']]
+
+                })
+            })
+    } else if (this.state.tog === 'on') {
+        this.setState({
+            tog: 'off'
+        })
+    }
+}
+
+    render() {
+        let toggleStyle = {
+            display: 'none'
+        }
+        if(this.state.tog === "on") {
+            toggleStyle = {
+                display: 'flex'
+            }
+        }
+        return (
+            <div>
+                <div className="buttoning">
+                <button onClick={this.myClick}>Get Weather</button>
+                </div>
+
+                <div style={toggleStyle}>
+                <ul className="outer">
+                        <li className="outli">Name: {this.state.name}</li>
+                        <br/>
+                        <li className="outli">Coordinates: 
+                        <ul className="inner">
+                            <li>longitude: {this.state.coordlon}</li>
+                            <li>Latitude: {this.state.coordlat}</li>
+                        </ul>
+                        </li>
+                        <br/>
+                        <li className="outli">Weather: 
+                        <ul className="inner">
+                            <li>ID: {this.state.weatherid}</li>
+                            <li>Main: {this.state.weathermain}</li>
+                            <li>Description: {this.state.weatherdescription}</li>
+                            <li>Icon: {this.state.weathericon}</li>
+                        </ul>
+                        </li>
+                        <br/>
+                        <li className="outli">Main: 
+                        <ul className="inner">
+                            <li>Temp: {this.state.maintemp}</li>
+                            <li>Feels_Like: {this.state.mainfeelslike}</li>
+                            <li>Temp_Min: {this.state.maintempmin}</li>
+                            <li>Temp_Max: {this.state.maintempmax}</li>
+                            <li>Pressure: {this.state.mainpressure}</li>
+                            <li>Humdity: {this.state.mainhumidity}</li>
+                        </ul>
+                        </li>
+                        <br/>
+                        <li className="outli">Visibility: {this.state.visibility}</li>
+                        <br/>
+                        <li className="outli">Wind: 
+                        <ul className="inner">
+                            <li>Speed: {this.state.windspeed}</li>
+                            <li>Deg: {this.state.winddeg}</li>
+                        </ul>
+                        </li>
+                        <br/>
+                        <li className="outli">Clouds: 
+                        <ul className="inner">
+                            <li>All: {this.state.cloudsall}</li>
+                        </ul>
+                        </li>
+                        <br/>
+                        <li className="outli">DT: {this.state.dt}</li>
+                        <br/>
+                        <li className="outli">Sys: 
+                        <ul className="inner">
+                            <li>Type: {this.state.systype}</li>
+                            <li>ID: {this.state.sysid}</li>
+                            <li>Country: {this.state.syscountry}</li>
+                            <li>Sunrise: {this.state.syssunrise}</li>
+                            <li>Sunset: {this.state.syssunset}</li>
+                        </ul>
+                        </li>
+                        <br/>
+                        <li className="outli">Timezone: {this.state.timezone}</li>
+                        <br/>
+                        <li className="outli">ID: {this.state.thisid}</li>
+                        <br/>
+                        <li className="outli">Cod: {this.state.cod}</li>
+                        <br/>
+                    </ul>
+                </div>
+                    
+            </div>
+        )
+    }
+}
+
+export default GiveWeather2;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import GiveWeather from './components/GiveWeather.js'
+//import GiveWeather from './components/GiveWeather.js'
+import GiveWeather2 from './components/GiveWeather2.js';
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
-        <GiveWeather/>
+        <GiveWeather2/>
       </div>
     )
   }
