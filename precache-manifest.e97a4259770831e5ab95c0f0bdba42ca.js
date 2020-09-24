61800e1ac66862fbfe7e017e2f903fadf652a27d
Julian Ranieri <jumpingafterrain@gmail.com>
2020-09-24 15:57:15 -0700

    Updates

---
+++ precache-manifest.e97a4259770831e5ab95c0f0bdba42ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6eee67640a0a1618fb93220f52fa62",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c61625e274055fc4f00",
+    "url": "./static/js/2.f70b1fee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f70b1fee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a140d6c9a431c9b897",
+    "url": "./static/js/main.d6b69e10.chunk.js"
+  },
+  {
+    "revision": "d1852769bb5ed6b18168",
+    "url": "./static/js/runtime-main.bc15de7f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.19873b34435cd3de207f2d15f6739a90.js"
+  "./precache-manifest.e97a4259770831e5ab95c0f0bdba42ca.js"
 );
 
 self.addEventListener('message', (event) => {
