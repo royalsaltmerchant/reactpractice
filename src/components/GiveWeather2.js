ca8df2c36d8792891b6fa7b78a3d254e86b57d03
Julian Ranieri <jumpingafterrain@gmail.com>
2020-10-21 16:07:35 -0700

    it works now, but there is no state parameter anymore

--- src/components/GiveWeather2.js
+++ src/components/GiveWeather2.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Select from 'react-select'
 import countryCodeData from './countrycode.json'
-import stateCodeData from './statecode.json'
+// import stateCodeData from './statecode.json'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 //url
 
 const urlbase = 'http://api.openweathermap.org/data/2.5/weather?appid=6b04193aa2d1531aa6072e2ba7eca3c8'
-const googlePredictionUrlBase = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?types=(cities)&key=AIzaSyDDWFmvc22fxUIqYAG3DNI9y9clzXbWdAY'
 const googleUrlBase = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCkzZnIUzlDVDEJ-aOKjOcKTPNLBdpGQHY&address='
 //class and bind
 class GiveWeather2 extends React.Component {
@@ -38,11 +38,10 @@ class GiveWeather2 extends React.Component {
     this.onChangeCityA = this.onChangeCityA.bind(this)
     this.onChangeCityB = this.onChangeCityB.bind(this)
     this.onChangeCountry = this.onChangeCountry.bind(this)
-    this.onChangeState = this.onChangeState.bind(this)
+    // this.onChangeState = this.onChangeState.bind(this)
     this.getLonLat = this.getLonLat.bind(this)
     this.getGoogle = this.getGoogle.bind(this)
     this.getUrl = this.getUrl.bind(this)
-    this.getLocationAuto = this.getLocationAuto.bind(this)
     }
 
 //Events
@@ -68,63 +67,55 @@ onChangeCountry(event) {
         }
     })
 }
-onChangeState(event) {
-    console.log(event)
-    this.setState({
-        stateSelect: event
-    }, () => {
-        if(event !== null) {
-            this.setState({
-                myState: event.value,
-                myStateLabel: event.label
-            }, () => {
-                console.log(this.state.myState)
-                this.setState({
-                    googleUrl: googleUrlBase + this.state.myStateLabel
-                }, () => {
-                    console.log(this.state.googleUrl)
-                    fetch(this.state.googleUrl)
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log(data)
-                        this.setState({
-                            googleStateLat: data['results'][0]['geometry']['location']['lat'],
-                            googleStateLon: data['results'][0]['geometry']['location']['lng']
-                        }, () => {
-                            console.log(this.state.googleStateLat, this.state.googleStateLon)
-                        })
-                    })
-                })
-            })
-        } else if(event === null) {
-            this.setState({
-                myState: null,
-                theirCity: '',
-                citySelect: null,
-            })
-        }
-    })
-}
+// onChangeState(event) {
+//     console.log(event)
+//     this.setState({
+//         stateSelect: event
+//     }, () => {
+//         if(event !== null) {
+//             this.setState({
+//                 myState: event.value,
+//                 myStateLabel: event.label
+//             }, () => {
+//                 console.log(this.state.myState)
+//                 this.setState({
+//                     googleUrl: googleUrlBase + this.state.myStateLabel
+//                 }, () => {
+//                     console.log(this.state.googleUrl)
+//                     fetch(this.state.googleUrl)
+//                     .then(response => response.json())
+//                     .then(data => {
+//                         console.log(data)
+//                         this.setState({
+//                             googleStateLat: data['results'][0]['geometry']['location']['lat'],
+//                             googleStateLon: data['results'][0]['geometry']['location']['lng']
+//                         }, () => {
+//                             console.log(this.state.googleStateLat, this.state.googleStateLon)
+//                         })
+//                     })
+//                 })
+//             })
+//         } else if(event === null) {
+//             this.setState({
+//                 myState: null,
+//                 theirCity: '',
+//                 citySelect: null,
+//             })
+//         }
+//     })
+// }
 onChangeCityA(event) {
     this.setState({
         theirCity: event.target.value
-    }, () => {
-        console.log(this.state.theirCity)
-        if(this.state.theirCity !== '') {
-        this.getLocationAuto()
-        } else {
-            this.setState({
-                predictionOptions: []
-            }, () => {
-                console.log(this.state.predictionOptions)
-            })
-        }
     })
 }
 onChangeCityB(event) {
     if(event !== null) {
         this.setState({
             citySelect: event
+        }, () => {
+            console.log(this.state.citySelect)
+            this.getLonLat()
         })
     } else {
         this.setState({
@@ -150,17 +141,9 @@ myClick() {
     console.log(this.state.citySelect)
 }
 getLonLat() {
-    if(this.state.myState === '' && this.state.myCountry !== '') {
+    if(this.state.myCountry !== '') {
         this.setState({
-            googleUrl: `${googleUrlBase}${this.state.citySelect.value},+${this.state.myCountry}`
-        }, () => {
-            console.log(this.state.googleUrl)
-            this.getGoogle()
-        })
-    }
-    else if(this.state.myState !== '') {
-        this.setState({
-            googleUrl: `${googleUrlBase}${this.state.citySelect.value},+${this.state.myState}`
+            googleUrl: `${googleUrlBase}${this.state.citySelect.label.replace(' ', '')}`
         }, () => {
             console.log(this.state.googleUrl)
             this.getGoogle()
@@ -238,51 +221,7 @@ getUrl() {
         }
     })
 }
-getLocationAuto() {
-    this.setState({
-        googleLocation: `${googlePredictionUrlBase}&components=country:${this.state.myCountry}&location=${this.state.googleStateLat},${this.state.googleStateLon}&radius=2000&input=${this.state.theirCity}`,
-        predictions: []
-    }, () => {
-        console.log(this.state.googleLocation)
-        fetch(this.state.googleLocation)
-        .then(response => response.json())
-        .then(data => {
-            // console.log(data)
-            for(var n = 0; n < data['predictions'].length; n++) {
-                var predictions = [data['predictions'][n]['terms'][0].value]
-                // console.log(predictions)
-                this.setState({
-                    predictions: [...this.state.predictions, predictions]
-                }, () => {
-                    this.setState({
-                        predictionOptions: [
-                                {
-                                    "label": this.state.predictions[0],
-                                    "value": this.state.predictions[0]
-                                },
-                                {
-                                    "label": this.state.predictions[1],
-                                    "value": this.state.predictions[1]
-                                },
-                                {
-                                    "label": this.state.predictions[2],
-                                    "value": this.state.predictions[2]
-                                },
-                                {
-                                    "label": this.state.predictions[3],
-                                    "value": this.state.predictions[3]
-                                },
-                                {
-                                    "label": this.state.predictions[4],
-                                    "value": this.state.predictions[4]
-                                }
-                        ]
-                    })
-                })
-            }
-        })
-    })
-}
+
 //end of events
 //Render
     render() {
@@ -303,9 +242,9 @@ getLocationAuto() {
         let countryForm = {
             display: ''
         }
-        let stateForm = {
-            display: 'none'
-        }
+        // let stateForm = {
+        //     display: 'none'
+        // }
         let cityForm = {
             display: 'none'
         }
@@ -313,21 +252,21 @@ getLocationAuto() {
             display: 'none'
         }
 
-        if(this.state.myCountry === 'US') {
-            stateForm = {
-                display: 'flex'
-            }
-        }
-        if(this.state.myCountry !== '' && this.state.myCountry !== 'US' && this.state.myCountry !== null) {
+        // if(this.state.myCountry === 'US') {
+        //     stateForm = {
+        //         display: 'flex'
+        //     }
+        // }
+        if(this.state.myCountry !== '' && this.state.myCountry !== null) {
             cityForm = {
                 display: 'flex'
             }
         }
-        if(this.state.myState !== '' && this.state.myState !== null) {
-            cityForm = {
-                display: 'flex'
-            }
-        }
+        // if(this.state.myState !== '' && this.state.myState !== null) {
+        //     cityForm = {
+        //         display: 'flex'
+        //     }
+        // }
         if(this.state.citySelect !== null) {
             radioUnits = {
                 display: 'flex'
@@ -365,16 +304,29 @@ getLocationAuto() {
                     <Select name="countryinput" value={this.state.countrySelect} onChange={this.onChangeCountry} options={countryCodeData} isClearable={true}/>
                 </div>
                 <br/>
-                <div className='stateform' style={stateForm}>
+                {/* <div className='stateform' style={stateForm}>
                     <label htmlFor="stateinput">State Name: </label>
                     <br/>
                     <Select name="stateinput" value={this.state.stateSelect} onChange={this.onChangeState} options={stateCodeData} isClearable={true}/>
                 </div>
-                <br/>
+                <br/> */}
                 <div className='cityform' style={cityForm} onChange={this.onChangeCityA}>
                     <label htmlFor="cityinput">City Name: </label>
                     <br/>
-                    <Select name="cityinput" value={this.state.citySelect} onChange={this.onChangeCityB} options={this.state.predictionOptions} isClearable={true} placeholder="Type City Name..."/>
+                    <GooglePlacesAutocomplete 
+                        selectProps={{
+                            value: this.state.citySelect,
+                            onChange: this.onChangeCityB,
+                          }}
+                        apiKey="AIzaSyDDWFmvc22fxUIqYAG3DNI9y9clzXbWdAY"
+                        autocompletionRequest={{
+                            types: ['(cities)'],
+                            componentRestrictions: {
+                                country:[this.state.myCountry]
+                            }
+                            
+                          }}
+                    />
                 </div>
                 <br/>
                 <div className="radiounits" style={radioUnits} onChange={this.onChangeRadio}>
